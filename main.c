19cfee828326fbb9dff9cb0c8be613c4f638333e
Devanshu <devanshuagarwal5@gmail.com>
2020-02-05 18:03:05 +0530

    Adding InitializeSerialPort method

--- main.c
+++ main.c
@@ -3,80 +3,88 @@
 #include <string.h>
 #define READ_TIMEOUT      500      // milliseconds
 
+BOOL InitializeSerialPort(HANDLE* hComm,char *portName);
+
 int main(){
 HANDLE hComm;  // Handle to the Serial port
     BOOL   Status; // Status
     DCB dcbSerialParams = { 0 };  // Initializing DCB structure
     COMMTIMEOUTS timeouts = { 0 };  //Initializing timeouts structure
-    char SerialBuffer[64] = { 0 }; //Buffer to send and receive data
+    char SerialBuffer[64] = "ATZ"; //Buffer to send and receive data
     DWORD BytesWritten = 0;          // No of bytes written to the port
     DWORD dwEventMask;     // Event mask to trigger
-    char  ReadData;        //temperory Character
+    unsigned char  ReadData;        //temperory Character
+    unsigned char tempChar;
     DWORD NoBytesRead;     // Bytes read by ReadFile()
     unsigned char loop = 0;
+    int index=0;
 
-    char pszPortName[10] = { 0 }; //com port id
-
+    char pszPortName[10] = "COM3"; //com port id
+    
 
 
     //Enter the com port id
-    printf("Enter the Com Port: ");
-    scanf("%s", pszPortName);
-
+    // printf("Enter the Com Port: ");
+    // scanf("%s", pszPortName);
+    Status= InitializeSerialPort(&hComm,pszPortName);
+    if(Status==FALSE){
+        system("pause");
+        return 0;
+    }
     //Open the serial com port
-    hComm = CreateFile(pszPortName, //friendly name
-                       GENERIC_READ | GENERIC_WRITE,      // Read/Write Access
-                       0,                                 // No Sharing, ports cant be shared
-                       NULL,                              // No Security
-                       OPEN_EXISTING,                     // Open existing port only
-                       0,                                 // Non Overlapped I/O
-                       NULL);                             // Null for Comm Devices
+    // hComm = CreateFile("COM3", //friendly name
+    //                    GENERIC_READ | GENERIC_WRITE,      // Read/Write Access
+    //                    0,                                 // No Sharing, ports cant be shared
+    //                    NULL,                              // No Security
+    //                    OPEN_EXISTING,                     // Open existing port only
+    //                    0,                                 // Non Overlapped I/O
+    //                    NULL);                             // Null for Comm Devices
 
-    if (hComm == INVALID_HANDLE_VALUE)
-    {
-        printf("\n Port can't be opened\n\n");
-        goto Exit2;
-    }
+    // if (hComm == INVALID_HANDLE_VALUE)
+    // {
+    //     printf("\n Port can't be opened\n\n");
+    //     goto Exit2;
+    // }
 
-    //Setting the Parameters for the SerialPort
-    dcbSerialParams.DCBlength = sizeof(dcbSerialParams);
+    // //Setting the Parameters for the SerialPort
+    // dcbSerialParams.DCBlength = sizeof(dcbSerialParams);
 
-    Status = GetCommState(hComm, &dcbSerialParams); //retreives  the current settings
-    if (Status == FALSE)
-    {
-        printf("\nError to Get the Com state\n\n");
-        goto Exit1;
-    }
+    // Status = GetCommState(hComm, &dcbSerialParams); //retreives  the current settings
+    // if (Status == FALSE)
+    // {
+    //     printf("\nError to Get the Com state\n\n");
+    //     goto Exit1;
+    // }
 
-    dcbSerialParams.BaudRate = CBR_38400;      //BaudRate = 9600
-    dcbSerialParams.ByteSize = 8;             //ByteSize = 8
-    dcbSerialParams.StopBits = ONESTOPBIT;    //StopBits = 1
-    dcbSerialParams.Parity = NOPARITY;      //Parity = None
+    // dcbSerialParams.BaudRate = CBR_38400;      //BaudRate = 9600
+    // dcbSerialParams.ByteSize = 8;             //ByteSize = 8
+    // dcbSerialParams.StopBits = ONESTOPBIT;    //StopBits = 1
+    // dcbSerialParams.Parity = NOPARITY;      //Parity = None
 
-    Status = SetCommState(hComm, &dcbSerialParams);
-    if (Status == FALSE)
-    {
-        printf("\nError to Setting DCB Structure\n\n");
-        goto Exit1;
-    }
+    // Status = SetCommState(hComm, &dcbSerialParams);
+    // if (Status == FALSE)
+    // {
+    //     printf("\nError to Setting DCB Structure\n\n");
+    //     goto Exit1;
+    // }
 
-    //Setting Timeouts
-    timeouts.ReadIntervalTimeout = 50;
-    timeouts.ReadTotalTimeoutConstant = 50;
-    timeouts.ReadTotalTimeoutMultiplier = 10;
-    timeouts.WriteTotalTimeoutConstant = 50;
-    timeouts.WriteTotalTimeoutMultiplier = 10;
-    if (SetCommTimeouts(hComm, &timeouts) == FALSE)
-    {
-        printf("\nError to Setting Time outs");
-        goto Exit1;
-    }
+    // //Setting Timeouts
+    // timeouts.ReadIntervalTimeout = 50;
+    // timeouts.ReadTotalTimeoutConstant = 50;
+    // timeouts.ReadTotalTimeoutMultiplier = 10;
+    // timeouts.WriteTotalTimeoutConstant = 50;
+    // timeouts.WriteTotalTimeoutMultiplier = 10;
+    // if (SetCommTimeouts(hComm, &timeouts) == FALSE)
+    // {
+    //     printf("\nError to Setting Time outs");
+    //     goto Exit1;
+    // }
 
 
     printf("\n\nEnter your message: ");
     scanf("%s", SerialBuffer);
-
-
+    strcat(SerialBuffer,"\r");
+    printf("Sending %s to COM port ", SerialBuffer);
     //Writing data to Serial Port
     Status = WriteFile(hComm,// Handle to the Serialport
                        SerialBuffer,            // Data to be written to the port
@@ -88,11 +96,17 @@ HANDLE hComm;  // Handle to the Serial port
         printf("\nFail to Written");
         goto Exit1;
     }
+    else
+    {
+        printf("\nWrite sucess");
+    }
+    
 
     //print numbers of byte written to the serial port
     printf("\nNumber of bytes written to the serail port = %d\n\n", BytesWritten);
 
-    //Setting Receive Mask
+    loop=0;
+//Setting Receive Mask
     Status = SetCommMask(hComm, EV_RXCHAR);
 
     if (Status == FALSE)
@@ -113,8 +127,8 @@ HANDLE hComm;  // Handle to the Serial port
     //Read data and store in a buffer
     do
     {
-        Status = ReadFile(hComm, &ReadData, sizeof(ReadData), &NoBytesRead, NULL);
-        SerialBuffer[loop] = ReadData;
+        Status = ReadFile(hComm, &tempChar, sizeof(tempChar), &NoBytesRead, NULL);
+        SerialBuffer[loop] = tempChar;
         ++loop;
     }
     while (NoBytesRead > 0);
@@ -125,7 +139,8 @@ HANDLE hComm;  // Handle to the Serial port
 
     //print receive data on console
     printf("\n\n");
-    int index = 0;
+
+    index = 0;
     for (index = 0; index < loop; ++index)
     {
         printf("%c", SerialBuffer[index]);
@@ -138,5 +153,48 @@ Exit1:
 Exit2:
     system("pause");
     return 0;
+}
+
+BOOL InitializeSerialPort(HANDLE * hComm, char *portName){
+     //Open the serial com port
+    BOOL   Status; // Status
+    DCB dcbSerialParams = { 0 };  // Initializing DCB structure
+
+    *hComm = CreateFile(portName, //friendly name
+                       GENERIC_READ | GENERIC_WRITE,      // Read/Write Access
+                       0,                                 // No Sharing, ports cant be shared
+                       NULL,                              // No Security
+                       OPEN_EXISTING,                     // Open existing port only
+                       0,                                 // Non Overlapped I/O
+                       NULL);                             // Null for Comm Devices
+
+   
+   
+    if (*hComm == INVALID_HANDLE_VALUE)
+    {
+        printf("\n Port can't be opened\n\n");
+        return FALSE;
+    }
+    dcbSerialParams.DCBlength = sizeof(dcbSerialParams);
+    Status = GetCommState(*hComm, &dcbSerialParams); //retreives  the current settings
+    if (Status == FALSE)
+    {
+        printf("\nError to Get the Com state\n\n");
+        CloseHandle(*hComm);
+        return FALSE;
+    }
+
+    dcbSerialParams.BaudRate = CBR_38400;      //BaudRate = 9600
+    dcbSerialParams.ByteSize = 8;             //ByteSize = 8
+    dcbSerialParams.StopBits = ONESTOPBIT;    //StopBits = 1
+    dcbSerialParams.Parity = NOPARITY;      //Parity = None
+
+    Status = SetCommState(*hComm, &dcbSerialParams);
+    if (Status == FALSE)
+    {
+        printf("\nError to Setting DCB Structure\n\n");
+        CloseHandle(*hComm);
+        return FALSE;
+    }
 }
 
